a71fafefc6da4df0fd7ed835882b161dd46b3eb3
LEEKH109 <dlrjsgml109@gmail.com>
2024-07-24 16:48:41 +0900

    [건희] 7.24 알고리즘 풀이(JavaScript&Java) - 프로그래머스

---
+++ 건희/7월 4주차/프로그래머스_level3_네트워크.java
@@ -0,0 +1,29 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/43162
+class Solution {
+    static int N;
+    static int map[][];
+    static boolean visited[];
+    public int solution(int n, int[][] computers) {
+        N = n;
+        int cnt = 0;
+        map = computers;
+        visited = new boolean [n];
+        int answer = 0;
+        for(int i = 0; i < N; i++){
+            if(!visited[i]){
+                dfs(i);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    
+    public static void dfs(int start){
+        visited[start] = true;
+        for(int i = 0; i < N; i++){
+            if(map[start][i] == 1 && !visited[i]){
+                dfs(i);
+            }
+        }
+    }
+}
