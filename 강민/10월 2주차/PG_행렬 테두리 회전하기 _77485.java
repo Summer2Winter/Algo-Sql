bbda99c96b8eaa927f9d68e2583c6a6d0489e68f
김강민 <mmm3657@naver.com>
2024-10-08 16:54:43 +0900

    [강민] 10.08 알고리즘 풀이(java) - 프로그래머스 77485

--- 강민/10월 2주차/PG_행렬 테두리 회전하기 _77485.java
+++ 강민/10월 2주차/PG_행렬 테두리 회전하기 _77485.java
@@ -30,7 +30,7 @@ class Solution {
             min = Math.min(min, temp2);
             for(int r=ex; r>sx+1; r--){
                 grid[r][ey] = grid[r-1][ey];
-                min = Math.min(min, grid[r-1][ey]);
+                min = Math.min(min, grid[r][ey]);
             }
             grid[sx+1][ey] = temp1;
 
@@ -46,7 +46,7 @@ class Solution {
             // 오른쪽
             for(int r=sx; r<ex-1; r++){
                 grid[r][sy] = grid[r+1][sy];
-                min = Math.min(min, grid[r+1][sy]);
+                min = Math.min(min, grid[r][sy]);
             }
             grid[ex-1][sy] = temp3;
             answer[k]=min;
@@ -54,3 +54,58 @@ class Solution {
         return answer;
     }
 }
+
+
+/*
+class Solution {
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[] answer = new int[queries.length];
+        // 배열 만들기
+        int[][] grid = new int[rows][columns];
+        int value = 1;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                grid[i][j] = value++;
+            }
+        }
+
+        // 배열 회전하기
+        for (int k = 0; k < queries.length; k++) {
+            int[] query = queries[k];
+            int sx = query[0] - 1, sy = query[1] - 1;
+            int ex = query[2] - 1, ey = query[3] - 1;
+
+            int temp = grid[sx][sy];
+            int min = temp;
+
+            // 왼쪽 세로
+            for (int i = sx; i < ex; i++) {
+                grid[i][sy] = grid[i + 1][sy];
+                min = Math.min(min, grid[i][sy]);
+            }
+
+            // 아래 가로
+            for (int i = sy; i < ey; i++) {
+                grid[ex][i] = grid[ex][i + 1];
+                min = Math.min(min, grid[ex][i]);
+            }
+
+            // 오른쪽 세로
+            for (int i = ex; i > sx; i--) {
+                grid[i][ey] = grid[i - 1][ey];
+                min = Math.min(min, grid[i][ey]);
+            }
+
+            // 위쪽 가로
+            for (int i = ey; i > sy + 1; i--) {
+                grid[sx][i] = grid[sx][i - 1];
+                min = Math.min(min, grid[sx][i]);
+            }
+            grid[sx][sy + 1] = temp;
+            answer[k] = min;
+        }
+
+        return answer;
+    }
+}
+ */
