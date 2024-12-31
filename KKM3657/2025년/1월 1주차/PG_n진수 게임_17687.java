59fb5cff7347a18d7cf4b56f6980810f84cc68a3
김강민 <mmm3657@naver.com>
2024-12-31 15:25:43 +0900

    [KKM3657] 12.31 알고리즘 풀이(java) - 프로그래머스 17687

--- KKM3657/2025년/1월 1주차/PG_n진수 게임_17687.java
+++ KKM3657/2025년/1월 1주차/PG_n진수 게임_17687.java
@@ -28,3 +28,25 @@ class Solution {
         return sb.toString();
     }
 }
+
+/*
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder result = new StringBuilder();
+        StringBuilder sequence = new StringBuilder(); // 전체 n진수 문자열
+        int value = 0;
+
+        // 필요한 길이: t * m (최소 이만큼 생성)
+        while (sequence.length() < t * m) {
+            sequence.append(Integer.toString(value++, n).toUpperCase());
+        }
+
+        // 튜브의 숫자만 추출
+        for (int i = 0; i < t; i++) {
+            result.append(sequence.charAt(i * m + (p - 1)));
+        }
+
+        return result.toString();
+    }
+}
+ */
