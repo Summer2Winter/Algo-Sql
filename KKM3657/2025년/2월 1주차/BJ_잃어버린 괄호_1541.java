a45ec5a11aef1f485bfbe58437ab54a58e6812af
김강민 <mmm3657@naver.com>
2025-02-04 16:51:57 +0900

    [KKM3657] 02.04 알고리즘 풀이(java) - 백준 1541

---
+++ KKM3657/2025년/2월 1주차/BJ_잃어버린 괄호_1541.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input, "+-", true);
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        while(st.hasMoreTokens()) {
+            String curr = st.nextToken();
+            // +,- 이면 push
+            if(curr.equals("+") || curr.equals("-"))
+                stack.push(curr);
+                // 숫자일 경우
+            else {
+                // 비어 있으면 push
+                if(stack.isEmpty())
+                    stack.push(curr);
+                else {
+                    // 최상단이 + 이면 더해서 push
+                    if(stack.peek().equals("+")) {
+                        String opcode = stack.pop(); // +
+                        String number = stack.pop(); // 숫자
+                        int value = Integer.parseInt(number) + Integer.parseInt(curr);
+                        stack.push(Integer.toString(value));
+                    }
+                    // 최상단이 - 이면 바로 push
+                    else {
+                        stack.push(curr);
+                    }
+                }
+            }
+        }
+        // System.out.println(stack.toString());
+        // 순서대로 계산하기
+        int answer = Integer.parseInt(stack.pollLast());
+        while(!stack.isEmpty()) {
+            String temp = stack.pollLast();
+            if(temp.equals("-"))
+                continue;
+            answer -= Integer.parseInt(temp);
+
+        }
+        System.out.println(answer);
+    }
+}
