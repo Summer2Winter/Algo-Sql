55e746af06eb611ed7776875fb91ce3caad502c7
김강민 <mmm3657@naver.com>
2025-02-09 01:13:52 +0900

    [KKM3657] 02.07 알고리즘 풀이(java) - 백준 15681

--- KKM3657/2025년/2월 1주차/BJ_트리와쿼리_15681.java
+++ KKM3657/2025년/2월 1주차/BJ_트리와쿼리_15681.java
@@ -4,67 +4,58 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
 
-class Node {
-    int node;
-    ArrayList<Integer> list = new ArrayList<>();
-
-    Node(int node) {
-        this.node = node;
-    }
-}
 public class Main {
     static int N,Q;
-    static Node[] tree;
+    static ArrayList<Integer>[] tree;
     static int[] answerArr;
     static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         int root = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
+
         answerArr = new int[N+1];
         visited = new boolean[N+1];
+        tree = new ArrayList[N+1];
 
-        tree = new Node[N+1];
         for(int i=1; i<=N; i++)
-            tree[i] = new Node(i);
+            tree[i] = new ArrayList<>();
 
         for(int i=1; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
 
-            tree[s].list.add(e);
-            tree[e].list.add(s);
+            tree[s].add(e);
+            tree[e].add(s);
         }
 
         // root 부터 후위 순위로 전체 개수 탐색
-        visited[root] = true;
         postOrder(root);
 
-        // 쿼리
-        for(int i=0; i<Q; i++){
-            System.out.println(answerArr[Integer.parseInt(br.readLine())]);
+        // 쿼리 처리
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Q; i++) {
+            sb.append(answerArr[Integer.parseInt(br.readLine())]).append("\n");
         }
+        System.out.print(sb);
     }
 
     public static int postOrder(int curr) {
-        ArrayList<Integer> child = tree[curr].list;
-        // 리프노드이면 돌아감
-        if(child.size() == 1) {
-            answerArr[curr] = 1;
-            return 1;
-        }
+
+        visited[curr] = true;
         // 현재 위치
         int result = 1;
+
         // 자식 이동
-        for(int next : child) {
+        for(int next : tree[curr]) {
             // 이미 방문했으면 넘어감
-            if(visited[next])
-                continue;
-            visited[next] = true;
-            result += postOrder(next);
+            if(!visited[next])
+                result += postOrder(next);
         }
         answerArr[curr] = result;
         return result;
